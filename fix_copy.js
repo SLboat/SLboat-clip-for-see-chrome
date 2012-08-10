3b67029ec3d540310ad2c62eb235dab9992df7e8
Sen <sl@sl088.com>
2012-08-10 13:02:04 +0000

    * br改为更标准的

--- fix_copy.js
+++ fix_copy.js
@@ -16,10 +16,10 @@ var funGetSelectTxt = function() {
 //--------------------------
 //传入了soure，title，url，在这里处理
 function fixtxt (source,title,url){
-	source = source.replace(/(\n|\r|\r\n)/g,"<br\\>$1");
+	source = source.replace(/(\n|\r|\r\n)/g,"<br />$1");
 	//处理标题加上标志
 	title+="<sup> 沿途见识</sup>"
 	var textout = "===" +title + "<ref>"+url+"</ref>===\r\n"+source;
-	textout=textout+"<br\\>\r\n";
+	textout=textout+"<br />\r\n";
 	return textout;
 }
