4934fde1d1bf6ac37d6d09cf9be55a7664eca9af
Sen <sl@sl088.com>
2012-12-28 11:21:57 +0000

    修改了获取标题机制

--- getsel.js
+++ getsel.js
@@ -1,8 +1,8 @@
 //添加事件钩子，当服务端请求的时候响应
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     if (request.method == "getSelection"){
-	  var eleTitle=document.getElementsByTagName("title")[0]
-	  var titlestr = (eleTitle && eleTitle.innerHTML)? eleTitle.innerHTML : "未起名标题";
+	  var titlestr=(document.title=="")?"无标题见识":document.title;//检测是否为空一起都在这里
+	  //遣送回去数据
       sendResponse({data: window.getSelection().toString(), title: titlestr, url: window.location.href});
 	}
     else
