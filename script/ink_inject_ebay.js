d55aeaaf295a179fb6891927026b72adddc053b0
Sen <sl@sl088.com>
2013-05-07 15:39:07 +0000

    * 香港小包需要拆开一些

--- script/ink_inject_ebay.js
+++ script/ink_inject_ebay.js
@@ -1,7 +1,7 @@
 //当前光标写入文字
 function ink_go_ebay(ink, curr_pos){
 		//注入香港小包文字，如果可能的话
-		jQuery("#trackingTable .boxtext[id^='carrierName']").val("HongKong Post");
+		jQuery("#trackingTable .boxtext[id^='carrierName']").val("Hong Kong Post"); //香港小包看起来，要拆开来
 
  }
 
