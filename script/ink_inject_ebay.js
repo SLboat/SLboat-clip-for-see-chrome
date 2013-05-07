e9e943a846360b0bfde4fd5596cdd8bed2c40ff2
Sen <sl@sl088.com>
2013-05-07 15:59:19 +0000

    * 注入ebay更新，自动按按钮

--- script/ink_inject_ebay.js
+++ script/ink_inject_ebay.js
@@ -2,7 +2,10 @@
 function ink_go_ebay(ink, curr_pos){
 		//注入香港小包文字，如果可能的话
 		jQuery("#trackingTable .boxtext[id^='carrierName']").val("Hong Kong Post"); //香港小包看起来，要拆开来
+		if (jQuery("#trackingTable .boxtext[id^='trackingNumber']:first").val() != "") //如果第一个不是空白的
+		{
+				jQuery("#update1 .btn").click(); //点击下去，看起来是的
+		}
 
  }
-
 //交给主脚本去处理事件钩子，到这里结束
