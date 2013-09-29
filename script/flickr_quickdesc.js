4e43886b4c2c74c24fad105ca733e594416b8f74
SLboat <slboat@me.com>
2013-09-29 19:12:22 +0800

    * 回忆上次的标记内容

--- script/flickr_quickdesc.js
+++ script/flickr_quickdesc.js
@@ -1,8 +1,23 @@
 /* 快速添加标记 */
 $(document).bind('keydown', 'alt+q', function() {
-	//标记文字
-	var note_str = prompt("你想标记点啥？船长");
-	if (note_str != "") {
+	var old_desc = "";
+	//获取正常文字
+	if ($("#description_div textarea").length > 0) {
+		old_desc = $("#description_div textarea").val(); //编辑框提取
+	} else {
+		old_desc = $.trim($("#description_div").text()); //非编辑框尝试，这里看起来是很多p
+	}
+
+	if (typeof(old_desc) != undefined && old_desc == "") //最终判断
+	{
+		var note_str = prompt("还未标记,标记点啥?船长");
+	} else {
+		var note_str = prompt("已标记,换成啥?船长", old_desc);
+	}
+
+	//开始处理标记
+
+	if (note_str != "" && note_str != old_desc) { //避开没有变化的变化
 		//点击编辑
 		$("#description_div").click();
 		//输入文字
