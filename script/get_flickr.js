7d679aad2bd1d44f77346cd5851c6a3efb1f50ac
SLboat <slboat@me.com>
2013-10-01 17:17:23 +0800

    * 补充回去tag丢失的一个空格

--- script/get_flickr.js
+++ script/get_flickr.js
@@ -472,7 +472,7 @@ function render_per_link(urlimg, urllink, str_alt, no_url_work, desc) {
 			txt_out += "<!-- 来自图片文件:" + alt_name + " 的Flickr标记-->"; //去掉分号，确保空格，检索的关键-这是英文
 		}
 		//todo: 是否前面传入个ID玩意，为了好看呢<flickr id="">，修理被引用问题，还是去空格的好
-		txt_out += '<flickr alt=\"' + str_alt + '\" id=\"' + flickr_id + '\" link=\"' + urllink + '\" img=\"' + urlimg + '\"' + descstr + '>'; //标记所有的一切
+		txt_out += '<flickr alt=\"' + str_alt + '\" id=\"' + flickr_id + '\" link=\"' + urllink + '\" img=\"' + urlimg + '\"' + descstr + '> '; //标记所有的一切
 		//看看是否增加ID
 		if (flickr_id != null) //检测是否有ID，最好必须有个这样的玩意
 		{
