f5207c24343308e939b7d8f0f75056a354654e18
SLboat <slboat@me.com>
2013-10-13 22:01:03 +0800

    加入全部添加标记的数量

--- script/Flickr/Flickr_Set1PicDesc.js
+++ script/Flickr/Flickr_Set1PicDesc.js
@@ -1,16 +1,16 @@
 /* 快速添加标记 
-  * 用于一个对话框之下快速的完成添加标记 
-  */
+ * 用于一个对话框之下快速的完成添加标记
+ */
 
-  var Enable_SetLotPicsDesc = false; //是否已经开启了大量图片标记
+var Enable_SetLotPicsDesc = false; //是否已经开启了大量图片标记
 
 //主函数
 var flickr_add_tag = function() { //这里包括了绑定基本的快捷键
 
 	//检查是否需要全局标记
-	if (Enable_SetLotPicsDesc){
-		Flickr_pics_quick_mouse();
-		alert("船长！已经给全部的图片写入了标记！")
+	if (Enable_SetLotPicsDesc) {
+		var work_num = Flickr_pics_quick_mouse();
+		alert("船长！已经给全部的图片写入了标记！大概是" + work_num + "个呢！")
 		return true; //已经完成工作
 	}
 
