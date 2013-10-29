fcf2385d3f623cf07ec53a0910c7f4e7f937bc4c
Sen@PC <slboat@me.com>
2013-10-29 17:48:25 +0800

    * 修改描述tag的顺序 * 使用api自己的排序(显然截图不会有太多好运)

--- script/Flickr/Flickr_GetPicsByAPI.js
+++ script/Flickr/Flickr_GetPicsByAPI.js
@@ -111,7 +111,7 @@ function call_flickr_api_search(search_tag, is_inOrganize) {
 	var Requst_url = "?method=flickr.photos.search"; //基础搭建
 	Requst_url += "&api_key=" + flickr_api_key.api_key; //这样拼看起来好看点
 	Requst_url += "&user_id=" + user_id; //用户ID，减少数量
-	Requst_url += "&tags=" + encodeURIComponent(search_tag); //搜索标签，将来要变成utf8的
+	Requst_url += "&tags=" + search_tag; //搜索标签，将来要变成utf8的
 
 	//强制逆序...这里或许可以取代后面的转录逆序
 	/* 一种后逆序的处理或许是 
